4145cd81cf104868190ec63121b19991c89f4349
Matthew Raehl <matthew.raehl@gmail.com>
2021-09-11 17:47:10 -0400

    Initial commit

---
+++ src/main/java/com/caled/ApplicationMain.java
@@ -0,0 +1,28 @@
+package com.caled;
+
+import javax.swing.*;
+import asciiPanel.AsciiPanel;
+/**
+ * Hello world!
+ *
+ */
+public class ApplicationMain extends JFrame
+{
+    private static final long serialVersionUID = 1060623638149583738L;
+
+    private AsciiPanel terminal;
+
+    public ApplicationMain(){
+        super();
+        terminal = new AsciiPanel();
+        terminal.write("rl tutorial", 1, 1);
+        add(terminal);
+        pack();
+    }
+
+    public static void main(String[] args) {
+        ApplicationMain app = new ApplicationMain();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
+}
