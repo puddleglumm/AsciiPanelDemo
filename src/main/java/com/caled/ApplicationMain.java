0d4554d99f5fd518d114b18e79a214794514aa00
Matthew Raehl <matthew.raehl@gmail.com>
2021-09-12 10:02:00 -0400

    chapter02

--- src/main/java/com/caled/ApplicationMain.java
+++ src/main/java/com/caled/ApplicationMain.java
@@ -1,24 +1,42 @@
 package com.caled;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 import asciiPanel.AsciiPanel;
-/**
- * Hello world!
- *
- */
-public class ApplicationMain extends JFrame
-{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import com.caled.screens.Screen;
+import com.caled.screens.StartScreen;
+
+public class ApplicationMain extends JFrame implements KeyListener {
     private static final long serialVersionUID = 1060623638149583738L;
 
     private AsciiPanel terminal;
+    private Screen screen;
 
     public ApplicationMain(){
         super();
         terminal = new AsciiPanel();
-        terminal.write("rl tutorial", 1, 1);
         add(terminal);
         pack();
+        screen = new StartScreen();
+        addKeyListener(this);
+        repaint();
     }
+
+    public void repaint(){
+        terminal.clear();
+        screen.displayOutput(terminal);
+        super.repaint();
+    }
+
+    public void keyPressed(KeyEvent e) {
+        screen = screen.respondToUserInput(e);
+        repaint();
+    }
+
+    public void keyReleased(KeyEvent e) { }
+
+    public void keyTyped(KeyEvent e) { }
 
     public static void main(String[] args) {
         ApplicationMain app = new ApplicationMain();
---
+++ src/main/java/com/caled/screens/Screen.java
@@ -0,0 +1,10 @@
+package com.caled.screens;
+
+import java.awt.event.KeyEvent;
+import asciiPanel.AsciiPanel;
+
+public interface Screen {
+    void displayOutput(AsciiPanel terminal);
+
+    Screen respondToUserInput(KeyEvent key);
+}
