30f363192c91955fcdd8fbfeba27b76571a32462
craehl <caleb_the_epic@icloud.com>
2022-06-02 15:13:00 -0400

    new redirectSelection constructor

--- src/main/java/com/caled/redirectSelection.java
+++ src/main/java/com/caled/redirectSelection.java
@@ -10,6 +10,11 @@ public class redirectSelection implements Selection {
         this.title = title;
         this.redirect = screen;
     }
+    redirectSelection(String title, int screen, int reset) {
+        this.resets = new int[]{ reset };
+        this.title = title;
+        this.redirect = screen;
+    }
     redirectSelection(String title, int screen) {
         this.resets = new int[]{};
         this.title = title;
