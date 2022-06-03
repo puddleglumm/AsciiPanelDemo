4c5062e0f7341134495b334e326c5002b84ffd0d
puddleglumm <matthew.raehl@gmail.com>
2022-06-03 13:06:44 -0400

    Added MinesweeperScreen and dependency.

---
+++ src/main/java/com/caled/MinesweeperScreen.java
@@ -0,0 +1,19 @@
+package com.caled;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import com.github.puddleglumm.minesweeper.*;
+
+public class MinesweeperScreen implements Screen {
+
+    Board board = new Board();
+
+    @Override
+    public void tick(ScreenedApplication application, ArrayList<KeyEvent> inputs) {
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
